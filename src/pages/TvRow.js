971648b4f064f3217e1916195d0555e7ffb61049
Ala123Sobhan <ala123sobhan@gmail.com>
2020-11-29 19:53:40 -0500

    adding class component TvRows.js

--- src/pages/TvRow.js
+++ src/pages/TvRow.js
@@ -173,8 +173,8 @@ export class TvRow extends Component {
                         <h3> {this.props.tv.name} </h3>
                         <p>{this.props.tv.overview}</p>
 
-                        <buttons className="btn" onClick={this.viewTv} value="View"> View </buttons>
-                        <buttons className="btn" onClick={this.getRecommendation} value="View"> Get Recommendation </buttons>
+                        <button className="btn" onClick={this.viewTv} value="View"> View </button>
+                        <button className="btn" onClick={this.getRecommendation} value="View"> Get Recommendation </button>
 
                     </td>
                 </tr>
